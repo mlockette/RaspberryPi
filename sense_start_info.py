82ab870317b4bc00ac418f38325da0cd0cf13eb6
mlockette <michael@lockds.com>
2015-11-16 17:12:41 +0000

    Cleaned up sense_start_info.py, and added details on how and what I am doing throughout the code.

--- sense_start_info.py
+++ sense_start_info.py
@@ -1,5 +1,20 @@
 #sense_start_info.py
-#sensehat startup info display.
+#Built by Michael Lockette, Lockette Down Security LLC
+# SenseHat startup info for tightVNC display
+# and wlan IP Address displayed across LED pad for user use upon bootup.
+
+'''
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 from sense_hat import SenseHat
 import netifaces as ni
@@ -10,28 +25,30 @@ import re
 
 
 sense = SenseHat()
+'''
+I personally feel I can read the message across the LED pad with a low
+ low light setting.  That is what the line below changes.
+'''
 sense.low_light = True
-#sense.set_rotation(180)
+
 green = (0,255,0)
 
+'''
+Fetch the wlan0 ip address.  object 'ip' will hold the pi's wlan0 ip address
+ for further use.
+'''
 ni.ifaddresses('wlan0')
 ip = ni.ifaddresses('wlan0')[2][0]['addr']
 print (ip)
-#print (type(ip))
-#sense.show_message("Wlan ip is: ", text_colour=green)
-#sense.show_message(ip, text_colour=green)
-#subprocess.call(["ps", "ax"])
+
+#Next find the process id for Xthightvnc and record the 'port id'
 cmd = "ps ax"
 p = subprocess.Popen(cmd, shell=True, stdout=PIPE)
 i = 0
 
 while True or i > 7000:
     out = p.stdout.read()
-    #print(out)
     sout = out.decode(encoding='UTF-8')
-    #print(sout)
-    
-    #print(type(out))
     if out == '' and p.poll() != None:
         break
     if 'Xtightvnc' in sout:
@@ -42,9 +59,14 @@ while True or i > 7000:
         break
 vncport = port.group(0)
 print(vncport)
-#print(type(port))
 
-
+'''
+Lastly create loop for continous printing of the information collected.
+ This portion is not quite a finished product,
+ just like any project... its truely never done...
+ The while loop only iterates once through the code.
+ then waits for an input to occur.
+'''
 while True:
     sense.show_message(vncport, text_colour=green)
     sense.show_message("Wlan ip is: ", text_colour=green)
